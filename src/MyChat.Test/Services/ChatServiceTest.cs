a0a9b5e9c39c6d7204db1a9f501bc6ddfe6fdf0a
Vitor G. Isse de Oliveira <oliveira.vitor3@gmail.com>
2021-01-18 23:50:47 -0300

    Added xUnit

---
+++ src/MyChat.Test/Services/ChatServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using MyChat.Application.Dto.Entities;
+using MyChat.Application.Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyChat.Test.Services
+{
+    public class ChatServiceTest
+    {
+        [Fact]
+        public void FalseBot()
+        {
+
+            Func<Task> act = async () =>
+            {
+                var chatService = new ChatService(null, null);
+                var chatMessage = new ChatMessageDto
+                {
+                    ChatGroup = null,
+                    Message = "/unknowBot=paramIrrelevant",
+                    UserName = "ignoredUser"
+                };
+
+                await chatService.ProcessMessageAsync(chatMessage);
+            };
+
+
+            Assert.ThrowsAsync<InvalidOperationException>(act);
+        }
+    }
+}
